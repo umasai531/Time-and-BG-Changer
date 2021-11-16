1647f52557414ff6a2a9597e7b74f9379254916c
umasai531 <umasai531@gmail.com>
2021-11-16 21:44:33 +0530

    Updates

---
+++ precache-manifest.5f925800a571f7316c862bb1de657382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9985ca8a238459ff1537ecee28e5ba19",
+    "url": "/Time-and-BG-Changer/index.html"
+  },
+  {
+    "revision": "c7674a685a03d18950a1",
+    "url": "/Time-and-BG-Changer/static/css/main.005f7adb.chunk.css"
+  },
+  {
+    "revision": "f2eb34bde2e0743ffa6d",
+    "url": "/Time-and-BG-Changer/static/js/2.24637379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Time-and-BG-Changer/static/js/2.24637379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7674a685a03d18950a1",
+    "url": "/Time-and-BG-Changer/static/js/main.adedb338.chunk.js"
+  },
+  {
+    "revision": "342b1419ac8302598439",
+    "url": "/Time-and-BG-Changer/static/js/runtime-main.2686be5d.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Time-and-BG-Changer/precache-manifest.5f925800a571f7316c862bb1de657382.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Time-and-BG-Changer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
